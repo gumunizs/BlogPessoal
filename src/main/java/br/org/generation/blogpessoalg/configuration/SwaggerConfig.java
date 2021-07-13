aafe627a7f23a20ac9285f4042e83371fd3ab553
Gustavo Muniz <gustavomunizsantos@gmail.com>
2021-07-13 17:37:16 -0300

    Deploy inicial - Blog Pessoal 2

--- src/main/java/br/org/generation/blogpessoalg/configuration/SwaggerConfig.java
+++ src/main/java/br/org/generation/blogpessoalg/configuration/SwaggerConfig.java
@@ -2,8 +2,11 @@ package br.org.generation.blogpessoalg.configuration;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,51 +19,55 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-
 	@Bean
 	public Docket api() {
-	return new Docket(DocumentationType.SWAGGER_2)
-	.select()
-	.apis(RequestHandlerSelectors
-	.basePackage("br.org.generation.blogpessoalg.controller"))
-	.paths(PathSelectors.any())
-	.build()
-	.apiInfo(metadata())
-	.useDefaultResponseMessages(false)
-	.globalResponses(HttpMethod.GET, responseMessageForGET());
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+	        .select()
+			.apis(RequestHandlerSelectors
+			.basePackage("br.org.generation.blogpessoalg.controller"))
+			.paths(PathSelectors.any())
+			.build()
+			.apiInfo(metadata())
+			.useDefaultResponseMessages(false)
+			.globalResponses(HttpMethod.GET, responseMessageForGET());
+			
 	}
+
 	public static ApiInfo metadata() {
+
 		return new ApiInfoBuilder()
-		.title("API - Blog Pessoal")
-		.description("Projeto API Spring - Blog Pessoal")
-		.version("1.0.0")
-		.license("Apache License Version 2.0")
-		.licenseUrl("http://localhost:8080/swagger-ui/")
-		.contact(contact())
-		.build();
-		}
-		private static Contact contact() {
-		return new Contact("Gustavo Muniz",
-		"https://github.com/gumunizs",
-		"gustavomunizsantos@gmail.com");
-		}
-		private static List<Response> responseMessageForGET() {return new ArrayList<Response>() {
-		private static final long serialVersionUID = 1L;
-		{
-		add(new ResponseBuilder().code("200")
-		.description("Sucesso!").build());
-		add(new ResponseBuilder().code("201")
-		.description("Objeto Criado!").build());
-		add(new ResponseBuilder().code("401")
-		.description("Não Autorizado!").build());
-		add(new ResponseBuilder().code("403")
-		.description("Proibido!").build());
-		add(new ResponseBuilder().code("404")
-		.description("Não Encontrado!").build());
-		add(new ResponseBuilder().code("500")
-		.description("Erro!").build());
-		}
+			.title("Blog Pessoal")
+			.description("Projeto API Spring - Blog Pessoal")
+			.version("1.0.0")
+			.license("Apache License Version 2.0")
+			.licenseUrl("http://localhost:8080/swagger-ui/")
+			.contact(contact())
+			.build();
+	}
+
+	private static Contact contact() {
+
+		return new Contact("Gustavo Muniz", "https://github.com/gumunizs", "gustavomunizsantos@gmail.com");
+
+	}
+
+	private static List<Response> responseMessageForGET() {
+
+		return new ArrayList<Response>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new ResponseBuilder().code("200").description("Sucesso!").build());
+				add(new ResponseBuilder().code("201").description("Objeto Criado!").build());
+				add(new ResponseBuilder().code("401").description("Não Autorizado!").build());
+				add(new ResponseBuilder().code("403").description("Proibido!").build());
+				add(new ResponseBuilder().code("404").description("Não Encontrado!").build());
+				add(new ResponseBuilder().code("500").description("Erro!").build());
+			}
 		};
-		}
+
+	}
 
 }
