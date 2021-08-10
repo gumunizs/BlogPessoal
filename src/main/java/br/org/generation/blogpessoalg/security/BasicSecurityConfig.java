b4e3f1436cba77dffd14e8871040addbbf49ded7
Gustavo Muniz <gustavomunizsantos@gmail.com>
2021-08-10 15:33:51 -0300

    Consertando problema do Heroku

--- src/main/java/br/org/generation/blogpessoalg/security/BasicSecurityConfig.java
+++ src/main/java/br/org/generation/blogpessoalg/security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package br.org.generation.blogpessoalg.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,8 +38,17 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()
+        .antMatchers("/**").permitAll() 
+        .antMatchers("/usuarios/logar").permitAll()
+        .antMatchers("/usuarios/cadastrar").permitAll()
+        .antMatchers(HttpMethod.GET, "/postagens").permitAll()
+        .antMatchers(HttpMethod.GET, "/tema").permitAll()
+        .antMatchers(HttpMethod.POST, "/postagens").permitAll()
+        .antMatchers(HttpMethod.POST, "/tema").permitAll()
+        .antMatchers(HttpMethod.PUT, "/postagens").permitAll()
+        .antMatchers(HttpMethod.PUT, "/tema").permitAll()
+        .antMatchers(HttpMethod.DELETE, "/postagens").permitAll()
+        .antMatchers(HttpMethod.DELETE, "/tema").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement()
